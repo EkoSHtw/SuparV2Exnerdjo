1537680585d75864620efa485c6e0ff3c7855476
What a nice, fancy mustache <viettu_hoang@yahoo.de>
2017-01-08 15:55:13 +0100

    systemwechsel

--- app/src/main/java/de/suparv2exnerdjocokg/suparv2exnerdjo/DialogAddToDo.java
+++ app/src/main/java/de/suparv2exnerdjocokg/suparv2exnerdjo/DialogAddToDo.java
@@ -3,6 +3,10 @@ package de.suparv2exnerdjocokg.suparv2exnerdjo;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 /**
  * Created by v2 on 05.01.2017.
@@ -17,7 +21,24 @@ public class DialogAddToDo extends Dialog {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.dialog_add_todo);
 
+        Button confirm = (Button) findViewById(R.id.dialog_todo_button_confirm);
+        Button cancel = (Button) findViewById(R.id.dialog_todo_button_cancel);
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText nameEditText = (EditText) linearLayout.findViewById(R.id.dialog_add_todo_name);
+                String name = nameEditText.getText().toString();
+
+                EditText descriptionEditText = (EditText) linearLayout.findViewById(R.id.dialog_add_todo_description);
+                String description = descriptionEditText.getText().toString();
+
+                EditText tagEdittext = (EditText)linearLayout.findViewById(R.id.dialog_add_todo_tag);
+                String tag = tagEdittext.getText().toString();4
+            }
+        });
 
     }
 }
