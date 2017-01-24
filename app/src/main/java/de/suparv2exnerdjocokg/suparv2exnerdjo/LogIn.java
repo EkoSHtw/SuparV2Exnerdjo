bd01facc5df6381c3789f306185b5619132f1d08
A nice, fancy mustache <viettu_hoang@yahoo.de>
2017-01-24 19:46:59 +0100

    splash screen, nicht schön

--- app/src/main/java/de/suparv2exnerdjocokg/suparv2exnerdjo/LogIn.java
+++ app/src/main/java/de/suparv2exnerdjocokg/suparv2exnerdjo/LogIn.java
@@ -1,15 +1,12 @@
 package de.suparv2exnerdjocokg.suparv2exnerdjo;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import de.suparv2exnerdjocokg.suparv2exnerdjo.Route.Route;
 
 public class LogIn extends AppCompatActivity {
 
@@ -37,7 +34,7 @@ public class LogIn extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), Route.class);
+                Intent intent = new Intent(v.getContext(), Splash.class);
                 startActivity(intent);
             }
         });
